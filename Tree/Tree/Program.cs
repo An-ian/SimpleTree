3d1e0b583d858b3eff8d20bd737c76a2c7c3e27e
Ckan <90206168+An-ian@users.noreply.github.com>
2021-09-08 23:48:25 +0900

    Add files via upload

---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TreeStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>();
+            TreeNode<int> root = tree.CreateNode(0);
+            TreeNode<int> a1 = tree.CreateNode(1);
+            TreeNode<int> a2 = tree.CreateNode(2);
+            TreeNode<int> a3 = tree.CreateNode(3);
+            TreeNode<int> a4 = tree.CreateNode(4);
+            TreeNode<int> b1 = tree.CreateNode(5);
+            TreeNode<int> b2 = tree.CreateNode(6);
+            TreeNode<int> c1 = tree.CreateNode(7);
+            TreeNode<int> c2 = tree.CreateNode(8);
+            tree.AddChildNode(root, a1);
+            tree.AddChildNode(root, a2);
+            tree.AddChildNode(root, a3);
+            tree.AddChildNode(root, a4);
+
+            tree.AddChildNode(a1, b1);
+            tree.AddChildNode(a2, b2);
+            tree.AddChildNode(b1, c1);
+            tree.AddChildNode(b1, c2);
+            tree.PrintNode(root,0);
+        }
+    }
+}
---
+++ Tree/Tree/Tree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeStruct
+{
+   
+    public class TreeNode<T>
+    {
+        public TreeNode<T> leftChild;
+        public TreeNode<T> rightSibling;
+        public T data;
+    }
+    public class Tree<T> where T : new ()
+    {
+        
+        public Tree(){}
+        public void AddChildNode(TreeNode<T> parent, TreeNode<T> child)
+        {
+            if (parent.leftChild == null)
+            {
+                parent.leftChild = child;
+            }
+            else
+            {
+                TreeNode<T> tempNode = parent.leftChild;
+                while (tempNode.rightSibling != null)
+                {
+                    tempNode = tempNode.rightSibling;
+                }
+                tempNode.rightSibling = child;
+            }
+        }
+        public TreeNode<T> CreateNode(T input)
+        {
+            TreeNode<T> newTree = new TreeNode<T>();
+            newTree.leftChild = null;
+            newTree.rightSibling = null;
+            newTree.data = input;
+            return newTree;
+        }
+        public void PrintNode(TreeNode<T> parent, int depth)
+        {
+            for (int i = 0;i < depth;i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine($"{parent.data.ToString()}");
+            if (parent.leftChild !=null)
+            {
+                PrintNode(parent.leftChild, depth + 1);
+            }
+            if (parent.rightSibling != null)
+            {
+                PrintNode(parent.rightSibling, depth);
+            }
+            
+        }
+    }
+
+}
